1bebbb2f7fa76da3871f503a788e67a5cc7637b2
Anubhav-Ghosh1 <iamanubhav11@gmail.com>
2024-12-09 22:39:04 +0530

    Added Code or getting user account balance

---
+++ src/controllers/account.controller.js
@@ -0,0 +1,35 @@
+import { User } from "../models/user.model";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiResponse } from "../utils/apiResponse";
+import { ApiError } from "../utils/apiError";
+
+/*
+getAccountBalance: Fetches the current account balance for a user.
+addFunds: Allows users to add money to their account (e.g., via a linked bank or card).
+withdrawFunds: Processes withdrawals to a linked bank account.
+linkBankAccount: Allows users to link their bank account for UPI operations.
+*/
+
+const getAccountBalance = asyncHandler(async(req,res) => {
+    try
+    {
+        const id = req.user?._id;
+        if(!id)
+        {
+            return res.Transaction(400).json(new ApiResponse(400,{},"Id is required"));
+        }
+        const userDetails = await User.findById(id);
+        if(!userDetails)
+        {
+            return res.status(400).json(new ApiResponse(400,{},"Error while getting user details"));
+        }
+
+        return res.status(200).json(new ApiResponse(200,userDetails.bankAccounts[0],"Account balance fetched successfully"));
+    }
+    catch(e)
+    {
+        return res.status(500).json(new ApiResponse(500,{},"Error while fetching account details"));
+    }
+});
+
+export {getAccountBalance};
